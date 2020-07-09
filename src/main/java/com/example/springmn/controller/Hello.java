cd73d8272bac716e8f7de8fa606dfa991093b934
guihai <d6day66@126.com>
2020-07-09 12:43:30 +0800

    11

--- src/main/java/com/example/springmn/controller/Hello.java
+++ src/main/java/com/example/springmn/controller/Hello.java
@@ -1,13 +1,19 @@
 package com.example.springmn.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/hello")
 public class Hello {
+    @Autowired
+    HttpServletRequest request;
     @RequestMapping
     public String hello() {
-        return "Hello World!";
+       return request.getRequestURI();
+
     }
 }
