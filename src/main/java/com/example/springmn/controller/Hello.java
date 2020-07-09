7281bf8567f6fda3b491f54a5009d2e610f46881
guihai <d6day66@126.com>
2020-07-09 14:02:10 +0800

    55

--- src/main/java/com/example/springmn/controller/Hello.java
+++ src/main/java/com/example/springmn/controller/Hello.java
@@ -5,16 +5,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/hello")
 public class Hello {
     @Autowired
     HttpServletRequest request;
+
     @RequestMapping
     public String hello() {
-        String str = "你的ip是： ";
-       return str + request.getRequestURL().toString();
+        BufferedWriter iptxt = null;
+        String str1 = "你的ip是： ";
+        String str2 = request.getRequestURL().toString();
+        try {
 
+            iptxt = new BufferedWriter(new FileWriter("iptxt.txt",true));
+            iptxt.write(str1 + str2);
+            iptxt.newLine();
+            iptxt.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("出错了");
+        }finally {
+            try {
+                iptxt.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return str1 + str2;
     }
 }
