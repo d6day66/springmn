c40b8cd886794198888c83108f9666acebfee660
guihai <d6day66@126.com>
2020-07-09 14:42:39 +0800

    66

--- src/main/java/com/example/springmn/controller/Hello.java
+++ src/main/java/com/example/springmn/controller/Hello.java
@@ -8,6 +8,11 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 @RestController
 @RequestMapping("/hello")
@@ -17,26 +22,60 @@ public class Hello {
 
     @RequestMapping
     public String hello() {
+        TimeZone tz = TimeZone.getTimeZone("ETC/GMT-8");
+        TimeZone.setDefault(tz);
         BufferedWriter iptxt = null;
-        String str1 = "你的ip是： ";
-        String str2 = request.getRequestURL().toString();
+        String str3 = "yyyy-MM-dd HH:mm:ss";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(str3);
+        Date date = new Date();
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
+        String format = simpleDateFormat.format(date);
+
+        String str1 = "欢迎留下你的足迹，你的ip是： ";
+        String str2 = request.getRemoteAddr();
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+            if ("127.0.0.1".equals(ip) || "0:0:0:0:0:0:0:1".equals(ip)) {
+                //根据网卡取本机配置的IP
+                InetAddress inet = null;
+                try {
+                    inet = InetAddress.getLocalHost();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+                ip = inet.getHostAddress();
+            }
+        }
         try {
 
-            iptxt = new BufferedWriter(new FileWriter("iptxt.txt",true));
-            iptxt.write(str1 + str2);
+            iptxt = new BufferedWriter(new FileWriter("iptxt.txt", true));
+            iptxt.write(str1 + ip + " " + format);
             iptxt.newLine();
             iptxt.close();
 
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("出错了");
-        }finally {
+        } finally {
             try {
                 iptxt.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-        return str1 + str2;
+        return str1 + ip;
     }
 }
